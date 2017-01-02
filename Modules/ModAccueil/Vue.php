4038541d44bdc7282bdf1000fe5727b6c25324c3
MoazChaudhry <moaz.chaudry@hotmail.com>
2017-01-02 18:51:00 +0100

    Add files via upload

--- Modules/ModAccueil/Vue.php
+++ Modules/ModAccueil/Vue.php
@@ -1,9 +1,30 @@
+
 <?php
 class Vue {
 	static function accueil () {
 ?>
-		accueil
-<?php
+		<div class="row">
+			<div class="col-xs-12 col-md-12 bg-success border">
+				<h1> Séance de dédicace  </h1>
+				<center><h2> L'auteur du célèbre manga One Piece, Eiichiro Oda à Paris ! </h2></center>
+				<center><img src="http://adala-news.fr/wp-content/uploads/2016/04/Eiichiro-Oda.png" class="img-responsive"  alt="Eiichiro-Oda" ></center></br>
+				<p>Commencée en 1997, la série voit son 81ème tome sortir le 4 Janvien 2016 en France. Récemment, la série a réalisé un record incroyable ! Plus de  320 millions d'exemplaires de ces tomes publiés dans le monde ! Ce record a été recompensé par le comité Guinness World Record comme étant "le plus grand nombre d'exemplaires publiés pour la même série de bande dessinées par un auteur unique". </p>
+				<p>A l'occasion de la sortie du 81ème tome en France, Eiichiro Oda sera présent pour la <strong>première fois en Europe</strong> et plus précisément en <strong>France pour une grande séance dédicace</strong> ! Une occasion en OR pour les fans français pour rencontrer ce génie du manga.  </p>
+				<div class="col-md-4">
+					<img src="http://www.glenatmanga.com/img/cata/02couv/9782344018286-G.jpg" class="img-responsive"  alt="Eiichiro-Oda" >
+				</div>
+				<div class="col-md-8">
+					<h3>Dédicace à Fnac LA DEFENSE, Paris</h3>
+					<h4>Dates et horaires : </h4>
+					<p> <strong>Mercredi 04 Janvier 2017, de 10h30 à 12h30 et de 13h30 à 17h. </strong></p>
+					<p> <strong>Jeudi 05 Janvier 2017, de 10h30 à 12h30 et de 13h30 à 17h. </strong></p>
+					<p> <strong>Vendredi 06 Janvier 2017, de 10h30 à 12h30 et de 13h30 à 19h. </strong> </p>
+					<p> Pour pouvoir participer à cet événement, <strong> vous devez vous inscrire</strong> ( cliquez sur " s'inscrire " en haut à droite ). Les places sont limités.</p>
+					<p>Cet événement est gratuit, cependant un support de dédicace en rapport avec un produit de l'auteur est demandé .</p>
+				</div>
+			</div>
+		</div>
+	<?php
 	}
 }
 ?>
